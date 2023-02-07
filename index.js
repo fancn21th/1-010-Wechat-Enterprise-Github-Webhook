534f4f0e77b9971fe3314776cea580f4f12f233e
fancn21th <fancn21th@gmail.com>
2023-02-07 13:33:04 +0800

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-import { stringify, parse } from "./utils.js";
-
 module.exports.handler = async (event) => {
-  const _method = event.routeKey;
-  const _body = parse(event.body); // body
-
-  console.log({
-    _method,
-    _body,
-  });
-
   return {
     statusCode: 200,
-    body: stringify(
+    body: JSON.stringify(
       {
-        ok: true,
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
       },
       null,
       2
--- utils.js
+++
@@ -1,4 +0,0 @@
-const stringify = (obj) => JSON.stringify(obj, null, 2);
-const parse = (str) => JSON.parse(str || "{}");
-
-export { stringify, parse };
